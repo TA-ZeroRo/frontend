184732d8ff4d1c263fe2df0caa3bc7c1848fbdf6
mousepotato03 <seongjin2@kau.kr>
2025-10-10 12:31:39 +0900

    STYLE: splash 페이지 UI 수정

--- lib/presentation/screens/entry/splash_screen.dart
+++ lib/presentation/screens/entry/splash_screen.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:rive/rive.dart' hide Image;
 import '../../../core/theme/app_color.dart';
 import '../../routes/router_path.dart';
 
@@ -12,16 +11,30 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
-  bool _isAnimationLoaded = false;
-  RiveAnimationController? _controller;
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _fadeController;
+  late Animation<double> _fadeAnimation;
 
   @override
   void initState() {
     super.initState();
-    _controller = SimpleAnimation('Timeline 1');
 
-    // 2초 후 login 화면으로 이동
+    // Fade 애니메이션 설정
+    _fadeController = AnimationController(
+      duration: const Duration(milliseconds: 1200),
+      vsync: this,
+    );
+
+    _fadeAnimation = CurvedAnimation(
+      parent: _fadeController,
+      curve: Curves.easeIn,
+    );
+
+    // 애니메이션 시작
+    _fadeController.forward();
+
+    // 2초 후 로그인 화면으로 이동
     Future.delayed(const Duration(seconds: 2), () {
       if (mounted) {
         context.go(RoutePath.login);
@@ -29,56 +42,52 @@ class _SplashScreenState extends State<SplashScreen> {
     });
   }
 
-  void _onRiveInit(Artboard artboard) {
-    setState(() {
-      _isAnimationLoaded = true;
-    });
-
-    // 애니메이션이 로드된 후 1초 뒤에 애니메이션 시작
-    Future.delayed(const Duration(seconds: 1), () {
-      if (mounted) {
-        _controller?.isActive = true;
-      }
-    });
-  }
-
   @override
   void dispose() {
-    _controller?.dispose();
+    _fadeController.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
       backgroundColor: AppColors.background,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Expanded(
-              child: Stack(
+      body: Stack(
+        children: [
+          // 하단 이미지 (위쪽 모서리 둥글게)
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            height: screenHeight * 0.34,
+            child: ClipRRect(
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(70),
+                topRight: Radius.circular(70),
+              ),
+              child: Image.asset(
+                'assets/images/splash.png.png',
+                width: double.infinity,
+                fit: BoxFit.cover,
                 alignment: Alignment.center,
-                children: [
-                  // 애니메이션이 로드되지 않았을 때 로딩 인디케이터
-                  if (!_isAnimationLoaded)
-                    const CircularProgressIndicator(
-                      valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
-                    ),
-
-                  // Rive 애니메이션
-                  if (_controller != null)
-                    RiveAnimation.asset(
-                      'assets/animation/zeroro_ani.riv',
-                      controllers: [_controller!],
-                      onInit: _onRiveInit,
-                      fit: BoxFit.contain,
-                    ),
-                ],
               ),
             ),
-          ],
-        ),
+          ),
+          // 로고
+          Positioned(
+            top: screenHeight * 0.28,
+            left: 0,
+            right: 0,
+            child: FadeTransition(
+              opacity: _fadeAnimation,
+              child: Center(
+                child: Image.asset('assets/images/ZeroRo_logo.png', width: 320),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
